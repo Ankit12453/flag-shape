45eb99ebcd82c0c9260c86139c854634d42a4105
ANKIT KUNDU <53438946+Ankit12453@users.noreply.github.com>
2020-09-09 12:11:20 -0700

    Add files via upload

---
+++ shape.py
@@ -0,0 +1,32 @@
+from turtle import *
+speed(0.2)
+color("green")
+shape("turtle")
+forward(400)
+right(90)
+forward(100)
+right(90)
+forward(400)
+right(90)
+forward(200)
+right(90)
+forward(400)
+right(90)
+forward(200)
+right(90)
+forward(400)
+right(90)
+forward(300)
+right(90)
+forward(400)
+right(90)
+forward(200)
+right(90)
+forward(200)
+count = 0
+while(count < 360):
+    forward(0.88)
+    right(1)
+    count = count + 1
+
+
